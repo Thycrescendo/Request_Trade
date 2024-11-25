1e197621335386405c569c7dae4f2d69734cc139
thycrescendo <dmicholn@gmail.com>
2024-11-25 18:38:38 +0100

    changed index and configuraton

--- index.js
+++ index.js
@@ -127,4 +127,4 @@ if (process.env.GENERATE_PEER_PORT === "true") {
 
 const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
-  console.log(`listening at ${POR
+  console.log(`listening at 
