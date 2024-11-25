6755c7253f6c25dff5e48590656389e3d75f6d6f
thycrescendo <dmicholn@gmail.com>
2024-11-25 18:34:31 +0100

    changed index and configuraton

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool, redisUrl: REDIS_URL });
 cgit add .
-git commit -m "chh.join(__dirname, "client/dist")));
+git commit h.join(__dirname, "client/dist")));
 
 require("./routes/blockchain")(app, blockchain, pubsub);
 require("./routes/wallet")(
