234cbcca122dbbdb10630333c312463bc71cd368
thycrescendo <dmicholn@gmail.com>
2024-11-25 18:33:34 +0100

    changed index and configuraton

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool, redisUrl: REDIS_URL });
-const tranParser.json());
+const Parser.json());
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 require("./routes/blockchain")(app, blockchain, pubsub);
